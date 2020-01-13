77ae80c0fa74b88fd8e57b16f88229732e485556
Trai Nguyen <tmnguyen8@gmail.com>
2020-01-12 21:50:54 -0500

    added images

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Banner () {
+    return (
+        <div className="container">
+            <h1>Clicky Game!</h1>
+            <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ src/components/NavTabs.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class NavTabs extends Component {
+  // Setting the component's initial state
+  state = {
+    firstName: "",
+    lastName: "",
+    password: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    if (name === "password") {
+      value = value.substring(0, 15);
+    }
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!this.state.firstName || !this.state.lastName) {
+      alert("Fill out your first and last name please!");
+    } else if (this.state.password.length < 6) {
+      alert(
+        `Choose a more secure password ${this.state.firstName} ${this.state
+          .lastName}`
+      );
+    } else {
+      alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
+    }
+
+    this.setState({
+      firstName: "",
+      lastName: "",
+      password: ""
+    });
+  };
+
+  render() {
+    // Notice how each input has a `value`, `name`, and `onChange` prop
+    return (
+      <div className="row nav-bar">
+        <div className="col-4">
+          <h1>Clicky Game</h1>
+        </div>
+        <div className="col-4">
+          <h1>You have guessed...</h1>
+        </div>
+        <div className="col-4">
+          <h1>Score...</h1>
+        </div>  
+      </div>
+
+    );
+  }
+}
+
+export default NavTabs;
+
