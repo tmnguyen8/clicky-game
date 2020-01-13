dad8da294cfb267950175aaf51cc54b8426dec1c
Trai Nguyen <tnguyen@at-pac.com>
2020-01-13 11:45:34 -0500

    Updates

---
+++ precache-manifest.fd18887c689672bb70174ceeae7a961f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17e3f0e2f101fb1f0c17fe7f2cd5100",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "aeec1ed0a8c6efd36ad7",
+    "url": "/clicky-game/static/css/main.988a54f6.chunk.css"
+  },
+  {
+    "revision": "d4d6572993f2bff13b35",
+    "url": "/clicky-game/static/js/2.11b97ad7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.11b97ad7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aeec1ed0a8c6efd36ad7",
+    "url": "/clicky-game/static/js/main.598f1cf1.chunk.js"
+  },
+  {
+    "revision": "b8bec2693b12df90db98",
+    "url": "/clicky-game/static/js/runtime-main.dda3ecff.js"
+  }
+]);
